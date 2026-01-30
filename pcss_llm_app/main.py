84eb0fcde5bc3a683a68c1dc54fb53f1f0cbeca9
Marek Urbaniak <murbaniak76@gmail.com>
2026-01-30 12:15:50 +0100

    UI: Set application-wide window icon

--- pcss_llm_app/main.py
+++ pcss_llm_app/main.py
@@ -5,6 +5,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from pcss_llm_app.ui.main_window import MainWindow
 
 def main():
@@ -12,6 +13,12 @@ def main():
     
     # Optional: Set styling
     app.setStyle("Fusion")
+
+    # Set App Icon Global
+    logo_path = os.path.join(os.path.dirname(__file__), "..", "resources", "logo.png")
+    if os.path.exists(logo_path):
+        app.setWindowIcon(QIcon(logo_path))
+
     
     window = MainWindow()
     window.show()
